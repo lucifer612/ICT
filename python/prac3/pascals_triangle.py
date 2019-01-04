9a2c0444090354b5ac528233adbebd8371c781fa
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-01-04 17:25:07 +0530

    Add files via upload

---
+++ python/prac3/pascals_triangle.py
@@ -0,0 +1,16 @@
+#python program  to print pascals triangle
+row=0
+col=0
+space=0
+
+number=int(input("enter the lenght of pascals triangle :"))
+for row in range(0,number):
+    for space in range(0,number-row):
+        print(" ",end="")
+    k=1
+    for col in range(0,row+1):
+        print(" ",end="")
+        print(int(k),end="")
+        k=k*(row-col)/(col+1)
+    
+    print("\n")
