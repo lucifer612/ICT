b74a71a53bf43a0e0a20d802b33e6c73687f48a3
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-01-04 15:18:06 +0530

    Create 6_slicing.py

---
+++ python/prac3/6_slicing.py
@@ -0,0 +1,8 @@
+string1=input("enter the first string :")
+string2=input("enter the second string :")
+
+slice_s2=string2[:2]   # slicing first 2 characters of 2nd string 
+slice_s1=string1[:2]   #slicing second 2 characters of 1st string
+
+final_string=(slice_s2+string1[2:]+slice_s1+string2[2:])
+print(final_string)
